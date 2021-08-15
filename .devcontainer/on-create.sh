a07520a6b33fdfa820b5963b5ddaad16d3b5a1fc
Bart Robertson <bartr@microsoft.com>
2021-08-15 16:17:37 -0500

    updated script

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -10,7 +10,6 @@ git clone https://github.com/retaildevcrews/ngsa-app /workspaces/ngsa-app
 git clone https://github.com/microsoft/webvalidate /workspaces/webvalidate
 
 # copy grafana.db to /grafana
-sudo rm -f /grafana/grafana.db
 sudo cp deploy/grafanadata/grafana.db /grafana
 sudo chown -R 472:0 /grafana
 
