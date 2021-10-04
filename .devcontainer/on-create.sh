a9315e7d084583ce838feea9b82c75fcdba29182
Bart Robertson <bartr@microsoft.com>
2021-10-04 12:43:09 -0500

    updated pre-build

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -21,4 +21,9 @@ docker network create k3d
 k3d registry create registry.localhost --port 5500
 docker network connect k3d k3d-registry.localhost
 
+# update the base docker images
+docker pull mcr.microsoft.com/dotnet/sdk:5.0-alpine
+docker pull mcr.microsoft.com/dotnet/aspnet:5.0-alpine
+docker pull mcr.microsoft.com/dotnet/sdk:5.0
+
 echo "on-create complete" >> ~/status
