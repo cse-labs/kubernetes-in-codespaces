39ab46eb7d073d8beb0d84fe75bea03610b9439e
Kevin Shah <71272427+kev-ms@users.noreply.github.com>
2022-03-17 18:24:37 +0000

    updated codespaces

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -5,15 +5,15 @@
 echo "$(date)    on-create start" >> $HOME/status
 
 # clone repos
-git clone https://github.com/retaildevcrews/ngsa-app /workspaces/ngsa-app
+git clone https://github.com/cse-labs/imdb-app /workspaces/imdb-app
 git clone https://github.com/microsoft/webvalidate /workspaces/webvalidate
 
 # restore the repos
 dotnet restore /workspaces/webvalidate/src/webvalidate.sln
-dotnet restore /workspaces/ngsa-app/Ngsa.App.csproj
+dotnet restore /workspaces/imdb-app/imdb.csproj
 
 # copy grafana.db to /grafana
-sudo cp deploy/grafanadata/grafana.db /grafana
+sudo cp .devcontainer/grafana.db /grafana
 sudo chown -R 472:0 /grafana
 
 # make sure everything is up to date
@@ -21,6 +21,9 @@ sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get autoremove -y
 sudo apt-get clean -y
+
+# todo - build cli from kic gh image
+echo "building cli"
 
 # create local registry
 docker network create k3d
@@ -31,5 +34,9 @@ docker network connect k3d k3d-registry.localhost
 docker pull mcr.microsoft.com/dotnet/sdk:5.0-alpine
 docker pull mcr.microsoft.com/dotnet/aspnet:5.0-alpine
 docker pull mcr.microsoft.com/dotnet/sdk:5.0
+docker pull mcr.microsoft.com/dotnet/aspnet:6.0-alpine
+docker pull mcr.microsoft.com/dotnet/sdk:6.0
+docker pull ghcr.io/cse-labs/webv-red:latest
+docker pull ghcr.io/cse-labs/webv-red:beta
 
 echo "$(date)    on-create complete" >> $HOME/status
