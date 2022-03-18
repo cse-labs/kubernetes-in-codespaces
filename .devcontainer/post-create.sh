32a40948e5c99ae4452706dff1dd74041435e535
Bart Robertson <bartr@microsoft.com>
2022-03-18 05:18:05 +0000

    updated codespaces

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -6,8 +6,8 @@ echo "post-create start"
 echo "$(date)    post-create start" >> "$HOME/status"
 
 # update the repos
-git pull -C /workspaces/imdb-app
-git pull -C /workspaces/webvalidate
+git -C /workspaces/imdb-app pull
+git -C /workspaces/webvalidate pull
 
 echo "post-create complete"
 echo "$(date +'%Y-%m-%d %H:%M:%S')    post-create complete" >> "$HOME/status"
