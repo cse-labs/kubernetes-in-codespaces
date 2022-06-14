e35b2dbdad9d7fbc4275d13b79d6631cb4dea4be
bartr <bartr@microsoft.com>
2022-06-14 04:17:43 +0000

    updated cli

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -9,29 +9,22 @@ echo "$(date +'%Y-%m-%d %H:%M:%S')    on-create start" >> "$HOME/status"
 git clone https://github.com/cse-labs/imdb-app /workspaces/imdb-app
 git clone https://github.com/microsoft/webvalidate /workspaces/webvalidate
 
-export REPO_BASE=$PWD
-export PATH="$PATH:$HOME/bin"
-
-mkdir -p "$HOME/.ssh"
-mkdir -p "$HOME/bin"
-
-{
-    # add cli to path
-    echo "export PATH=\$PATH:\$HOME/bin"
-
-    echo "export REPO_BASE=$REPO_BASE"
-    echo "compinit"
-} >> "$HOME/.zshrc"
-
 # restore the repos
 dotnet restore /workspaces/webvalidate/src/webvalidate.sln
 dotnet restore /workspaces/imdb-app/src/imdb.csproj
 
-# make sure everything is up to date
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get autoremove -y
-sudo apt-get clean -y
+export REPO_BASE=$PWD
+export PATH="$PATH:$REPO_BASE/cli"
+
+mkdir -p "$HOME/.ssh"
+
+{
+    # add cli to path
+    echo "export PATH=\$PATH:\$REPO_BASE/cli"
+
+    echo "export REPO_BASE=$REPO_BASE"
+    echo "compinit"
+} >> "$HOME/.zshrc"
 
 # create local registry
 docker network create k3d
@@ -43,23 +36,20 @@ docker pull mcr.microsoft.com/dotnet/aspnet:6.0-alpine
 docker pull mcr.microsoft.com/dotnet/sdk:6.0
 docker pull ghcr.io/cse-labs/webv-red:latest
 
-echo "dowloading kic CLI"
-# download cli
-mkdir -p "$HOME/bin"
-cd "$HOME/bin" || exit
-
-tag=$(curl -s https://api.github.com/repos/retaildevcrews/akdc/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3)}')
-wget -O kic.tar.gz https://github.com/retaildevcrews/akdc/releases/download/$tag/kic-$tag-linux-amd64.tar.gz
-tar -zxvf kic.tar.gz
-rm -f kic.tar.gz
-
-cd "$OLDPWD" || exit
-
 echo "generating kic completion"
 kic completion zsh > "$HOME/.oh-my-zsh/completions/_kic"
 
 echo "creating k3d cluster"
 kic cluster rebuild
+
+# only run apt upgrade on pre-build
+if [ "$CODESPACE_NAME" = "null" ]
+then
+    sudo apt-get update
+    sudo apt-get upgrade -y
+    sudo apt-get autoremove -y
+    sudo apt-get clean -y
+fi
 
 echo "on-create complete"
 echo "$(date +'%Y-%m-%d %H:%M:%S')    on-create complete" >> "$HOME/status"
